24b11e1141f9171c25d7abdb5f4d1e173a0bf176
Jason Ricardo Hernández <69086663+jasrhm93@users.noreply.github.com>
2020-08-04 10:36:06 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Assignment: Caching the Inverse of a Matrix| Encontrar la inversa de una matriz
+# This function creates a special "matrix" object that can cache its inverse.
+# Esta funcion crea una matriz a la cual se le puede calcular su inversa
 
-## Write a short comment describing this function
+# Creates a matrix that can cache it's inverse
+# Crea una matriz y mantiene su inversa en su cache
+#
+# Args:
+#   x: A matrix (Optional)
+#
+# Returns:
+#   A matrix with functions to get/set value & get/set inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  #Cached inverse of matrix
+  #Inversa de una matriz en la memoria
+  inv <- NULL
+  #setter for matrix
+  #set para la matriz
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  # getter for matrix
+  # get para la matriz
+  get <- function() {x}
+  ## getter/setter for matrix inverse
+  #setInverse y getInverse para la matriz inversa
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
+
+
+# Computes the inverse of a matrix. If the inverse has already been
+# calculated before, the cached inverse is returned.
+# La siguiente funcion calcula la matriz inversa. Si la inversa ya se calculo con
+# la función anterior, retorna el cache de su inversa
+# Args:
+#   x: A matrix
+#   ...: Extra arguments
+#
+# Returns:
+#   The inverse of the matrix
+# Devuelve la inversa de la matriz 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  
+  # return cached matrix inverse if it's been already computed
+  # Devuelve la inversa de la matriz si ya se calculo
+  if (!is.null(inv)) {
+    message("inverse is cached")
+    return(inv)
+  }
+  
+  # compute inverse of matrix 
+  # Calcula la inversa de la matriz
+  m <- x$get()
+  inv <- solve(m, ...)
+  
+  x$setinv(inv)
+  
+  # return inverse of matrix
+  # Devuelve la inversa de la matriz
+  return(inv)
 }
